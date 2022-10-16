7bdaf40ed24aa0b0154eeb0a841d9afdc1a13b7e
gdarlan <gabriel.darlan88@gmail.com>
2022-10-16 18:02:43 -0300

    Seção 13: Content Negotiation

--- rest-with-spring-boot-and-kotlin-gabriel-darlan/src/main/kotlin/br/com/gdarlan/controllers/PersonController.kt
+++ rest-with-spring-boot-and-kotlin-gabriel-darlan/src/main/kotlin/br/com/gdarlan/controllers/PersonController.kt
@@ -3,8 +3,8 @@ package br.com.gdarlan.controllers
 import br.com.gdarlan.data.vo.v1.PersonVO
 import br.com.gdarlan.data.vo.v2.PersonVO as PersonVOV2
 import br.com.gdarlan.services.PersonServices
+import br.com.gdarlan.util.MediaType
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
@@ -16,35 +16,36 @@ class PersonController {
     private lateinit var service: PersonServices
 
 
-    @GetMapping(produces = [MediaType.APPLICATION_JSON_VALUE])
+    @GetMapping(produces = [MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML])
     fun findAll(): List<PersonVO> {
         return service.findAll()
     }
 
-    @GetMapping(value = ["/{id}"], produces = [MediaType.APPLICATION_JSON_VALUE])
+    @GetMapping(value = ["/{id}"], produces = [MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML])
     fun findById(@PathVariable(value = "id") id: Long): PersonVO {
         return service.findById(id)
     }
 
     @PostMapping(
-        consumes = [MediaType.APPLICATION_JSON_VALUE],
-        produces = [MediaType.APPLICATION_JSON_VALUE]
+        consumes = [MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML],
+        produces = [MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML]
     )
     fun create(@RequestBody person: PersonVO): PersonVO {
         return service.create(person)
     }
 
-    @PostMapping(value = ["/v2"],
-        consumes = [MediaType.APPLICATION_JSON_VALUE],
-        produces = [MediaType.APPLICATION_JSON_VALUE]
+    @PostMapping(
+        value = ["/v2"],
+        consumes = [MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML],
+        produces = [MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML]
     )
     fun createV2(@RequestBody person: PersonVOV2): PersonVOV2 {
         return service.createV2(person)
     }
 
     @PutMapping(
-        consumes = [MediaType.APPLICATION_JSON_VALUE],
-        produces = [MediaType.APPLICATION_JSON_VALUE]
+        consumes = [MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML],
+        produces = [MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML]
     )
     fun update(@RequestBody person: PersonVO): PersonVO {
         return service.update(person)
@@ -52,7 +53,7 @@ class PersonController {
 
     @DeleteMapping(
         value = ["/{id}"],
-        produces = [MediaType.APPLICATION_JSON_VALUE]
+        produces = [MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML]
     )
     fun delete(@PathVariable(value = "id") id: Long): ResponseEntity<*> {
         service.delete(id)
--- rest-with-spring-boot-and-kotlin-gabriel-darlan/src/main/kotlin/br/com/gdarlan/data/vo/v1/PersonVO.kt
+++ rest-with-spring-boot-and-kotlin-gabriel-darlan/src/main/kotlin/br/com/gdarlan/data/vo/v1/PersonVO.kt
@@ -4,15 +4,15 @@ import com.fasterxml.jackson.annotation.JsonIgnore
 import com.fasterxml.jackson.annotation.JsonProperty
 import com.fasterxml.jackson.annotation.JsonPropertyOrder
 
-@JsonPropertyOrder("id", "address","first_name", "last_name","gender")
+//@JsonPropertyOrder("id", "address","first_name", "last_name","gender")
 data class PersonVO(
     var id: Long = 0,
-    @field:JsonProperty("first_name")
+//    @field:JsonProperty("first_name")
     var firstName: String = "",
-    @field:JsonProperty("last_name")
+//    @field:JsonProperty("last_name")
     var lastName: String = "",
     var address: String = "",
-    @field:JsonIgnore
+//    @field:JsonIgnore
     var gender: String = "",
 )
 
